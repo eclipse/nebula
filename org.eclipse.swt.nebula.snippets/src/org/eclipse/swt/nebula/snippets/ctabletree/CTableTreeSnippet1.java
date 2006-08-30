4786fe8716032c8fec366d044c02c1853574d555
jdowdall <jdowdall>
2006-08-30 17:12:17 +0000

    *** empty log message ***

--- org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/ctabletree/CTableTreeSnippet1.java
+++ org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/ctabletree/CTableTreeSnippet1.java
@@ -1,13 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+/****************************************************************************
+ * Copyright (c) 2006 Jeremy Dowdall
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ *    Jeremy Dowdall <jeremyd@aspencloud.com> - initial API and implementation
+ *****************************************************************************/
 package org.eclipse.swt.nebula.snippets.ctabletree;
 
 
--- org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/ctabletree/MultiLineCell.java
+++ org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/ctabletree/MultiLineCell.java
@@ -1,3 +1,13 @@
+/****************************************************************************
+ * Copyright (c) 2006 Jeremy Dowdall
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jeremy Dowdall <jeremyd@aspencloud.com> - initial API and implementation
+ *****************************************************************************/
 package org.eclipse.swt.nebula.snippets.ctabletree;
 
 import java.text.BreakIterator;
