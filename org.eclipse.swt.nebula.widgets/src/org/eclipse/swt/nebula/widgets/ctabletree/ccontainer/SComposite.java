37e3a185c73c5cf4ea395839d1dcda0ef33dc0bb
jdowdall <jdowdall>
2006-10-13 16:12:31 +0000

    added javadoc; changed background setting; changed style constants

--- org.eclipse.swt.nebula.widgets/src/org/eclipse/swt/nebula/widgets/ctabletree/ccontainer/SComposite.java
+++ org.eclipse.swt.nebula.widgets/src/org/eclipse/swt/nebula/widgets/ctabletree/ccontainer/SComposite.java
@@ -32,6 +32,25 @@ import org.eclipse.swt.widgets.Listener;
  */
 public class SComposite extends Canvas implements Listener {
 
+	/**
+	 * Style setting indicating that Border should NOT be drawn
+	 */
+	public static int NONE		= 0;
+	/**
+	 * Style setting indicating that a Border should be drawn
+	 */
+	public static int BORDER 	= 1 << 0;
+	/**
+	 * Style setting indicating that the Border should be drawn with a Flat appearance,
+	 * otherwise the border will have a subtle raised appearance.
+	 */
+	public static int FLAT		= 1 << 1;
+	/**
+	 * Style setting indicating that the Border should be drawn with Square corners,
+	 * otherwise the corners will be rounded.
+	 */
+	public static int SQUARE	= 1 << 2;
+	
 	private static Color WHITE = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE);
 	
 	private Color	borderColor;
@@ -55,8 +74,9 @@ public class SComposite extends Canvas implements Listener {
 		super(parent, SWT.NONE);
 
 		borderStyle = style;
-
-		setTrueBackground(parent.getBackground());
+		borderWidth = 0;
+		
+		updateBackground();
 		setBackground(parent.getBackground());
 		
 		// setup graphics support
@@ -70,8 +90,22 @@ public class SComposite extends Canvas implements Listener {
 		addListener(SWT.Paint, this);
 	}
 	
+	public int getBorderStyle() {
+		return borderStyle;
+	}
+
+	public int getBorderWidth() {
+		return borderWidth;
+	}
+
+	public void handleEvent(Event event) {
+		if(event.type == SWT.Paint) {
+			paintControl(event.gc);
+		}
+	}
+	
 	private void paintControl(GC gc) {
-		if((borderStyle & SWT.BORDER) != 0) {
+		if((borderStyle & BORDER) != 0) {
 			if(advancedGraphics) {
 				gc.setAntialias(SWT.ON);
 			}
@@ -80,8 +114,8 @@ public class SComposite extends Canvas implements Listener {
 			
 			Rectangle r = getClientArea();
 			
-			if((borderStyle & SWT.SIMPLE) != 0) {
-				if((borderStyle & SWT.FLAT) == 0) {
+			if((borderStyle & SQUARE) != 0) {
+				if((borderStyle & FLAT) == 0) {
 					gc.setForeground(WHITE);
 					gc.drawLine(r.x+1,r.y+1, r.x+1,r.y+r.height-3);
 					gc.drawLine(r.x+1,r.y+1, r.x+r.width-3,r.y+1);
@@ -89,7 +123,7 @@ public class SComposite extends Canvas implements Listener {
 				gc.setForeground(borderColor);
 				gc.drawRectangle(r.x,r.y, r.width-1,r.height-1);
 			} else {
-				if((borderStyle & SWT.FLAT) == 0) {
+				if((borderStyle & FLAT) == 0) {
 					gc.setForeground(WHITE);
 					gc.drawLine(r.x+1,r.y+1, r.x+1,r.y+r.height-3);
 					gc.drawLine(r.x+1,r.y+1, r.x+r.width-3,r.y+1);
@@ -99,7 +133,18 @@ public class SComposite extends Canvas implements Listener {
 			}
 		}
 	}
-
+	
+	/**
+	 * The setBackground method of an SComposite does not actually set its own
+	 * background color, as a typical Composite does, but rather the color of its border.
+	 * This enables its parent to set the background color on all child controls with an 
+	 * identical call: <code>((Control) child).setBackground(color)</code>, and have the
+	 * SComposite do its job of drawing a color coordinated border.<br />
+	 * To set the actual background of the SComposite, see the <code>updateBackground</code>
+	 * method. 
+	 * @see #updateBackground()
+	 * {@inheritDoc}
+	 */
 	public void setBackground(Color color) {
 		if(getBackground().getRGB().equals(color.getRGB())) {
 			if(normalBorderColor == null) {
@@ -110,32 +155,29 @@ public class SComposite extends Canvas implements Listener {
 			borderColor = color;
 		}
 	}
-
-	public void setNormaBorderColor(Color color) {
-		normalBorderColor = color;
-	}
 	
-	public void setTrueBackground(Color color) {
-		super.setBackground(color);
-	}
-	
-	public int getBorderStyle() {
-		return borderStyle;
-	}
-	
+	/**
+	 * Set the style of border to be drawn.<br />
+	 * Valid Styles:
+	 * <ul>
+	 * 	<li>NONE (default)</li>
+	 * 	<li>BORDER</li>
+	 * 	<li>FLAT</li>
+	 * 	<li>SQUARE</li>
+	 * </ul>
+	 * @param style
+	 * @see #setLayout(Layout)
+	 */
 	public void setBorderStyle(int style) {
 		if(borderStyle != style) {
-			GridLayout layout = (GridLayout) getLayout();
-			setLayout(layout);
 			borderStyle = style;
-			redraw();
 		}
 	}
 
-	public int getBorderWidth() {
-		return borderWidth;
-	}
-	
+	/**
+	 * Set the line width of the border to be drawn.
+	 * @param width
+	 */
 	public void setBorderWidth(int width) {
 		if(borderWidth != width) {
 			borderWidth = width;
@@ -144,19 +186,22 @@ public class SComposite extends Canvas implements Listener {
 	}
 	
 	/**
-	 * sets the layout ONLY if layout is a GridLayout
-	 * and makes sure there's room for the borders
-	 *  1. Square: (borderWidth+1) pixels left & top, borderWidth pixels right & bottom
-	 *  2. Flat Square: borderWidth pixels all around
-	 *  3. Round:  (borderWidth+1) pixels all around (arc gets cut otherwise)
-	 *  4. Flat Round:  (borderWidth+1) pixels all around
+	 * Sets the layout ONLY if layout is a GridLayout
+	 * and makes sure that there is room for the borders by setting the margins as
+	 * shown below:
+	 * <ol>
+	 *  <li>BORDER:  (borderWidth+1) pixels all around (arc gets cut otherwise)</li>
+	 *  <li>BORDER | FLAT:  (borderWidth+1) pixels all around</li>
+	 *  <li>BORDER | SQUARE: (borderWidth+1) pixels left & top, borderWidth pixels right & bottom</li>
+	 * 	<li>BORDER | SQUARE | FLAT: borderWidth pixels all around</li>
+	 * </ol>
 	 */
 	public void setLayout(Layout layout) {
 		if((layout != null) && (layout instanceof GridLayout)) {
 			GridLayout gridlayout = (GridLayout) layout;
 
-			if((borderStyle & SWT.BORDER) != 0) {
-				if((borderStyle & SWT.SIMPLE) != 0) {
+			if((borderStyle & BORDER) != 0) {
+				if((borderStyle & SQUARE) != 0) {
 					if(gridlayout.marginHeight < (borderWidth+1)) {
 						if(gridlayout.marginTop 	< (borderWidth+1)) gridlayout.marginTop    = (borderWidth+1); 
 						if(gridlayout.marginBottom 	< (borderWidth+1)) gridlayout.marginBottom = (borderWidth+1); 
@@ -166,7 +211,7 @@ public class SComposite extends Canvas implements Listener {
 						if(gridlayout.marginRight < (borderWidth+1)) gridlayout.marginRight = (borderWidth+1); 
 					}
 				} else 
-					if((borderStyle & SWT.FLAT) != 0) {
+					if((borderStyle & FLAT) != 0) {
 						if(gridlayout.marginHeight < borderWidth) {
 							if(gridlayout.marginTop 	< borderWidth) gridlayout.marginTop    = borderWidth; 
 							if(gridlayout.marginBottom 	< borderWidth) gridlayout.marginBottom = borderWidth; 
@@ -191,9 +236,19 @@ public class SComposite extends Canvas implements Listener {
 		}
 	}
 	
-	public void handleEvent(Event event) {
-		if(event.type == SWT.Paint) {
-			paintControl(event.gc);
-		}
+	/**
+	 * Set the color of the border to be drawn when the cell is NOT selected.<br />
+	 * @param color
+	 */
+	public void setNormalBorderColor(Color color) {
+		normalBorderColor = color;
+	}
+	
+	/**
+	 * This method to sets the actual background of this SComposite
+	 * to match that of its parent.  Use when the parent's background has been changed.
+	 */
+	public void updateBackground() {
+		super.setBackground(getParent().getBackground());
 	}
 }
