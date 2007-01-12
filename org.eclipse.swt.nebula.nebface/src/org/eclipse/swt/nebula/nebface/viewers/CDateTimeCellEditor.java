62a91dab473c0673f0b769ec6d6dcfe91c59d9e4
jdowdall <jdowdall>
2007-01-12 03:50:03 +0000

    Initial Commit

---
+++ org.eclipse.swt.nebula.nebface/src/org/eclipse/swt/nebula/nebface/viewers/CDateTimeCellEditor.java
@@ -0,0 +1,87 @@
+package org.eclipse.swt.nebula.nebface.viewers;
+
+import java.util.Date;
+
+import org.eclipse.core.runtime.Assert;
+import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.nebula.widgets.cdatetime.CDT;
+import org.eclipse.swt.nebula.widgets.cdatetime.CDateTime;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class CDateTimeCellEditor extends CellEditor {
+
+	/**
+	 * The CDateTime control; initial selection is the time of instantiation
+	 */
+	private CDateTime cdt;
+
+    /**
+     * Creates a new date/time cell editor parented under the given control.
+     *
+     * @param parent the parent control
+     */
+    public CDateTimeCellEditor(Composite parent) {
+        this(parent, CDT.DROP_DOWN);
+    }
+
+    /**
+     * Creates a new date/time cell editor parented under the given control.
+     *
+     * @param parent the parent control
+     * @param style the style bits
+     */
+    public CDateTimeCellEditor(Composite parent, int style) {
+        super(parent, style);
+    }
+
+    /**
+     * Creates a new date/time cell editor parented under the given control.
+     *
+     * @param parent the parent control
+     * @param style the style bits
+     */
+    public CDateTimeCellEditor(Composite parent, String pattern) {
+        super(parent, CDT.DROP_DOWN);
+        cdt.setPattern(pattern);
+    }
+
+    protected Control createControl(Composite parent) {
+		cdt = new CDateTime(parent, CDT.DROP_DOWN | getStyle());
+		cdt.addSelectionListener(new SelectionAdapter() {
+			public void widgetDefaultSelected(SelectionEvent e) {
+		        fireApplyEditorValue();
+		        deactivate();
+			}
+		});
+        cdt.addTraverseListener(new TraverseListener() {
+            public void keyTraversed(TraverseEvent e) {
+                if(e.detail == SWT.TRAVERSE_ESCAPE) {
+                    deactivate();
+                }
+            }
+        });
+		cdt.setFont(parent.getFont());
+		cdt.setBackground(parent.getBackground());
+		return cdt;
+	}
+
+	protected Object doGetValue() {
+		return cdt.getSelection();
+	}
+
+	protected void doSetFocus() {
+		cdt.setOpen(true);
+	}
+
+	protected void doSetValue(Object value) {
+        Assert.isTrue(cdt != null && (value instanceof Date));
+        cdt.setSelection((Date) value);
+	}
+
+}
