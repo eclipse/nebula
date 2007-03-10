a502f861fbe60c4c3d6f8687918a56e2bfcd37f4
jdowdall <jdowdall>
2007-03-10 12:49:24 +0000

    CTree has been moved to its own plugin (org.eclipse.nebula.widgets.ctree)

--- org.eclipse.swt.nebula.widgets/src/org/eclipse/swt/nebula/widgets/ctree/CTreeColumn.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.swt.nebula.widgets.ctree;
-
-public class CTreeColumn extends AbstractColumn {
-
-	public CTreeColumn(AbstractContainer parent, int style) {
-		super(parent, style);
-	}
-
-}
