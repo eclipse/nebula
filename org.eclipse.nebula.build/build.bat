8c04fd72c7ca68702005cefce06e3f1903d69b15
cgross <cgross>
2007-03-20 22:00:55 +0000

    *** empty log message ***

--- org.eclipse.nebula.build/build.bat
+++ org.eclipse.nebula.build/build.bat
@@ -1,7 +1,7 @@
-set PATH=C:\Program Files\Java\jdk1.5.0_04\bin;%PATH%;c:\build\bin
-set ECLIPSE_HOME=c:\eclipse33m5\eclipse
-
-cd\build
-
-java -cp %ECLIPSE_HOME%\startup.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile masterbuild.xml -DbaseLocation=%ECLIPSE_HOME% -logfile log.txt -Dpassword=xxxxxx
-
+set ECLIPSE_HOME=d:\nebula_build\eclipse33m5\eclipse
+set JAVA="D:\ibm-jdk5.0-sr3"
+set PATH=%JAVA%\bin;%PATH%;%ECLIPSE_HOME%\plugins\org.apache.ant_1.6.5\lib;bin\
+cvs -d:pserver:anonymous@dev.eclipse.org:/cvsroot/technology checkout org.eclipse.swt.nebula/org.eclipse.nebula.build
+cd org.eclipse.swt.nebula/org.eclipse.nebula.build
+java -cp %ECLIPSE_HOME%\startup.jar;bin\jsch-0.1.32.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile masterbuild.xml -DbaseLocation=%ECLIPSE_HOME% -logfile ../../log.txt -Dpassword=soldier*
+cd ../../
