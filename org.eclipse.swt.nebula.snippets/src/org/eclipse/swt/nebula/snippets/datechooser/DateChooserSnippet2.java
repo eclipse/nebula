442fa1def4640fe1b41eccb9c326469a578b6b92
ewuillai <ewuillai>
2007-03-26 21:44:03 +0000

    Publish first Nebula release

---
+++ org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/datechooser/DateChooserSnippet2.java
@@ -0,0 +1,31 @@
+package org.eclipse.swt.nebula.snippets.datechooser;
+
+import org.eclipse.nebula.widgets.datechooser.DateChooser;
+import org.eclipse.nebula.widgets.datechooser.DateChooserTheme;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Snippet 2 for DateChooser : border, blue theme, week numbers and footer.
+ */
+public class DateChooserSnippet2 {
+	public static void main(String[] args) {
+		Display display = new Display();
+    Shell shell = new Shell(display);
+    shell.setLayout(new GridLayout());
+
+    // Create a CCalendar with all default settings.
+    DateChooser cal = new DateChooser(shell, SWT.BORDER);
+    cal.setTheme(DateChooserTheme.BLUE);
+    cal.setWeeksVisible(true);
+    cal.setFooterVisible(true);
+
+    shell.open();
+    while ( ! shell.isDisposed() ) {
+    	if (!display.readAndDispatch()) display.sleep();
+    }
+    display.dispose();
+	}
+}
