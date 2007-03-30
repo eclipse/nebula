9eed4e0188588269601bc0e52d99676dec06a28d
cgross <cgross>
2007-03-30 15:32:19 +0000

    *** empty log message ***

--- org.eclipse.nebula.build/build.bat
+++ org.eclipse.nebula.build/build.bat
@@ -1,5 +1,5 @@
 set HOME=d:\nebula_build
-set ECLIPSE_HOME=d:\nebula_build\eclipse33m5\eclipse
+set ECLIPSE_HOME=d:\nebula_build\eclipse33m6\eclipse
 set JAVA="D:\ibm-jdk5.0-sr3"
 set PATH=%JAVA%\bin;%PATH%;%ECLIPSE_HOME%\plugins\org.apache.ant_1.6.5\lib;.
 set CVSROOT=:pserver:anonymous@dev.eclipse.org:/cvsroot/technology
@@ -7,4 +7,4 @@ d:
 cd\nebula_build\org.eclipse.swt.nebula
 ..\cvs update -C org.eclipse.nebula.build/masterbuild.xml
 cd org.eclipse.nebula.build
-java -cp %ECLIPSE_HOME%\startup.jar;..\..\jsch-0.1.32.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile masterbuild.xml -DbaseLocation=%ECLIPSE_HOME% -logfile ../../log.txt -Dpassword=XXXX
+java -cp %ECLIPSE_HOME%/plugins/org.eclipse.equinox.launcher_1.0.0.v20070319.jar;..\..\jsch-0.1.32.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile masterbuild.xml -DbaseLocation=%ECLIPSE_HOME% -logfile ../../log.txt -Dpassword=XXXX
