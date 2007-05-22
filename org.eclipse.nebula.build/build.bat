dc78467882679f6d684e068a5487cb409f102cc7
cgross <cgross>
2007-05-22 16:57:13 +0000

    *** empty log message ***

--- org.eclipse.nebula.build/build.bat
+++ org.eclipse.nebula.build/build.bat
@@ -6,5 +6,6 @@ set CVSROOT=:pserver:anonymous@dev.eclipse.org:/cvsroot/technology
 d:
 cd\nebula_build\org.eclipse.swt.nebula
 ..\cvs update -C org.eclipse.nebula.build/masterbuild.xml
+..\cvs update -C org.eclipse.nebula.build/common.xml
 cd org.eclipse.nebula.build
 java -cp %ECLIPSE_HOME%/plugins/org.eclipse.equinox.launcher_1.0.0.v20070319.jar;..\..\jsch-0.1.32.jar org.eclipse.core.launcher.Main -application org.eclipse.ant.core.antRunner -buildfile masterbuild.xml -DbaseLocation=%ECLIPSE_HOME% -logfile ../../log.txt -Dpassword=XXXX
