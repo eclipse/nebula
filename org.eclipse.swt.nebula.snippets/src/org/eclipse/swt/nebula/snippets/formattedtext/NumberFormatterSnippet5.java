9cea5fc76e4638e12957f9bffe75239f858622c2
ewuillai <ewuillai>
2007-06-24 16:38:25 +0000

    Correction of bug 192525

--- org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/formattedtext/NumberFormatterSnippet5.java
+++ org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/formattedtext/NumberFormatterSnippet5.java
@@ -4,15 +4,20 @@ import java.util.Locale;
 
 import org.eclipse.nebula.widgets.formattedtext.FormattedText;
 import org.eclipse.nebula.widgets.formattedtext.NumberFormatter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 /**
  * Snippet 5 for NumberFormatter : default Locale US, given fixed mask, no
- * default value.
+ * default value. Label and button used to get and display the current value
+ * in the Text widget.
  */
 public class NumberFormatterSnippet5 {
 	public static void main(String[] args) {
@@ -21,12 +26,28 @@ public class NumberFormatterSnippet5 {
     shell.setLayout(new GridLayout());
 
     Locale.setDefault(Locale.US);
-    FormattedText text = new FormattedText(shell, SWT.BORDER | SWT.SINGLE);
-    text.setFormatter(new NumberFormatter("###,###,##0.00"));
-    text.getControl().setOrientation(SWT.LEFT_TO_RIGHT);
+    final FormattedText text = new FormattedText(shell, SWT.BORDER | SWT.SINGLE);
+    text.setFormatter(new NumberFormatter("-###,###,###"));
     GridData data = new GridData();
-    data.widthHint = 100;
+    data.widthHint = 200;
     text.getControl().setLayoutData(data);
+
+    final Label result = new Label(shell, SWT.NONE);
+    data = new GridData();
+    data.widthHint = 200;
+    result.setLayoutData(data);
+
+    Button button = new Button(shell, SWT.PUSH);
+    button.setText("Get value");
+    button.addSelectionListener(new SelectionListener() {
+			public void widgetDefaultSelected(SelectionEvent e) {
+      }
+
+			public void widgetSelected(SelectionEvent e) {
+				Object value = text.getValue();
+				result.setText(value != null ? value.toString() : "null");
+      }
+    });
 
     shell.open();
     while ( ! shell.isDisposed() ) {
