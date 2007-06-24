e5b004aa4200ed2f2355eca3a50cb0eff2c06e09
ewuillai <ewuillai>
2007-06-24 16:39:24 +0000

    Correction of bug 193922

--- org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/formattedtext/DateFormatterSnippet2.java
+++ org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/formattedtext/DateFormatterSnippet2.java
@@ -9,7 +9,7 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
 /**
- * Snippet 2 for DateFormatter : fixed mask, no default value.
+ * Snippet 2 for DateFormatter : fixed mask yyyy/MM/dd, no default value.
  */
 public class DateFormatterSnippet2 {
 	public static void main(String[] args) {
@@ -18,7 +18,7 @@ public class DateFormatterSnippet2 {
     shell.setLayout(new GridLayout());
 
     FormattedText text = new FormattedText(shell, SWT.BORDER);
-    text.setFormatter(new DateFormatter("dd/MM/yyyy"));
+    text.setFormatter(new DateFormatter("yyyy/MM/dd"));
     GridData data = new GridData();
     data.widthHint = 70;
     text.getControl().setLayoutData(data);
