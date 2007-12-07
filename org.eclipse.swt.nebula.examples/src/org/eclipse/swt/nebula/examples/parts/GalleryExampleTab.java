9d0f995cd31a08d4d6b84d2332c90c2a1909ea03
rnicolas <rnicolas>
2007-12-07 15:18:07 +0000

    Cleanup

--- org.eclipse.swt.nebula.examples/src/org/eclipse/swt/nebula/examples/parts/GalleryExampleTab.java
+++ org.eclipse.swt.nebula.examples/src/org/eclipse/swt/nebula/examples/parts/GalleryExampleTab.java
@@ -26,7 +26,6 @@ import org.eclipse.swt.nebula.examples.ExamplesView;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Scale;
 
