ed9c2e53e49cd473dd9cdbc4d1825e44bb382901
dorme <dorme>
2008-01-25 21:22:45 +0000

    Refactoring into new approved format

--- org.eclipse.swt.nebula.widgets/src/org/eclipse/swt/nebula/widgets/compositetable/ColumnControlListener.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.swt.nebula.widgets.compositetable;
-
-public abstract class ColumnControlListener {
-    public abstract void columnResized(int resizedColumnNumber,
-            int resizedColumnWidth, int columnToTheRightOfResizedColumnWidth);
-
-    public abstract void columnMoved(int[] newColumnOrder);
-}
