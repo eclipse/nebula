b9a36873db579f6bf687d07f378fb2f122491ca9
ecrumhorn <ecrumhorn>
2008-06-24 20:44:58 +0000

    Removed 1.5 ref

--- org.eclipse.nebula.widgets.calendarcombo/src/org/eclipse/nebula/widgets/calendarcombo/Tester.java
+++ org.eclipse.nebula.widgets.calendarcombo/src/org/eclipse/nebula/widgets/calendarcombo/Tester.java
@@ -1,6 +1,7 @@
 package org.eclipse.nebula.widgets.calendarcombo;
 
-import java.util.Calendar;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
@@ -18,57 +19,26 @@ public class Tester {
 		shell.setText("Button Composite Tester");
 		shell.setSize(200, 400);
 		
+		// allow other date formats than default
+		class Settings extends DefaultSettings {
+
+			public List getAdditionalDateFormats() {
+				List additional = new ArrayList();
+				additional.add("MMddyy");
+				return additional;
+			}
+			
+		}
+		
 		shell.setLayout(new FillLayout());
 		Composite inner = new Composite(shell, SWT.None);
 		GridLayout gl = new GridLayout(1, true);		
 		inner.setLayout(gl);
 
-		Calendar before = Calendar.getInstance();
-		before.add(Calendar.MONTH, -1);
-		before.set(Calendar.DATE, 15);		
-
-		Calendar after = Calendar.getInstance();
-		after.add(Calendar.MONTH, 1);
-		after.set(Calendar.DATE, 15);		
-		
 		Label foo = new Label(inner, SWT.NONE);
-		foo.setText("Whatever date is set on this one...");
-		final CalendarCombo one = new CalendarCombo(inner, SWT.RIGHT_TO_LEFT);
-		one.setDisallowBeforeDate(before);
-		one.setDisallowAfterDate(after);
-		Calendar too = Calendar.getInstance();
-		//too.set(Calendar.MONTH, 10);
-		//too.set(Calendar.DATE, 1);		
-		
-		one.setDate(too.getTime());
-		Label foo2 = new Label(inner, SWT.NONE);
-		foo2.setText("Will be the start for this one...");
-		CalendarCombo two = new CalendarCombo(inner, SWT.READ_ONLY, one);
-		
-		new Label(inner, SWT.NONE);
-		CalendarCombo three = new CalendarCombo(inner, SWT.NONE);
-		CalendarCombo four = new CalendarCombo(inner, SWT.NONE);
-		four.setEnabled(false);
-		
-		one.addCalendarListener(new ICalendarListener() {
-			public void popupClosed() {
-			}
-
-			public void dateChanged(Calendar date) {
-				if (date != null) {
-					System.err.println("Date in getDate() " + one.getDate().getTime());
-					System.err.println("Date as event " + date.getTime().toString());
-				}
-				else
-					System.err.println("null");
-			}	
-			
-			public void dateRangeChanged(Calendar start, Calendar end) {
-				System.err.println("Range from " + start.getTime() + " to " + end.getTime());
-			}
-		});
-		
-		//Utils.centerDialogOnScreen(shell);
+		foo.setText("Test");
+		new CalendarCombo(inner, SWT.NONE, new Settings(), null);
+	
 		shell.open();
 
 		while (!shell.isDisposed ()) {
