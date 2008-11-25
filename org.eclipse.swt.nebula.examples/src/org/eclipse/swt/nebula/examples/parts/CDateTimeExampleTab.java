d37e5797a1e5635c479c77cd12891ec8dc7c8431
rnicolas <rnicolas>
2008-11-25 08:20:46 +0000

    Added code for standalone application Added headers

--- org.eclipse.swt.nebula.examples/src/org/eclipse/swt/nebula/examples/parts/CDateTimeExampleTab.java
+++ org.eclipse.swt.nebula.examples/src/org/eclipse/swt/nebula/examples/parts/CDateTimeExampleTab.java
@@ -1,4 +1,12 @@
-package org.eclipse.swt.nebula.examples.parts;
+/*******************************************************************************
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    jdowdall - initial API and implementation
+ *******************************************************************************/ package org.eclipse.swt.nebula.examples.parts;
 
 import java.util.Locale;
 
