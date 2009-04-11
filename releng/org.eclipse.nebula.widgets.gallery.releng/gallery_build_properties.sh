d69f01b063399405836a3879d6264b7521ff2ebf
rnicolas <rnicolas>
2009-04-11 10:47:34 +0000

    Moved plugin

---
+++ releng/org.eclipse.nebula.widgets.gallery.releng/gallery_build_properties.sh
@@ -0,0 +1,6 @@
+nebulaProjectId="gallery"
+relengProjectPath="org.eclipse.swt.nebula/org.eclipse.nebula.widgets.gallery.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="0.5.0";
+buildType="N"
