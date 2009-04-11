8836282d1672670cde43b76927fab052186eefd1
rnicolas <rnicolas>
2009-04-11 13:14:52 +0000

    moved to releng

--- releng/org.eclipse.nebula.widgets.gallery.releng/gallery_build_properties.sh
+++ releng/org.eclipse.nebula.widgets.gallery.releng/gallery_build_properties.sh
@@ -1,5 +1,5 @@
 nebulaProjectId="gallery"
-relengProjectPath="org.eclipse.swt.nebula/org.eclipse.nebula.widgets.gallery.releng"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.gallery.releng"
 projectid="technology.nebula.$nebulaProjectId"
 sub="nebula.$nebulaProjectId"
 version="0.5.0";
