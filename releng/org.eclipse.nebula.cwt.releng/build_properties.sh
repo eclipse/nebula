84a30407d87f1d4926ee4579ea0a9b9565001146
jdowdall <jdowdall>
2009-04-11 22:52:13 +0000

    *** empty log message ***

---
+++ releng/org.eclipse.nebula.cwt.releng/build_properties.sh
@@ -0,0 +1,6 @@
+nebulaProjectId="cwt"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.cwt.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="0.9.0";
+buildType="N"
