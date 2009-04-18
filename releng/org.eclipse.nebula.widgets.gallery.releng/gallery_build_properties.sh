f8cc30585324d599ecfb34665f74ffaa22bd483f
rnicolas <rnicolas>
2009-04-18 08:58:20 +0000

    Added tests support

--- releng/org.eclipse.nebula.widgets.gallery.releng/gallery_build_properties.sh
+++ releng/org.eclipse.nebula.widgets.gallery.releng/gallery_build_properties.sh
@@ -4,3 +4,5 @@ projectid="technology.nebula.$nebulaProjectId"
 sub="nebula.$nebulaProjectId"
 version="0.5.0";
 buildType="N"
+# Add this line if no tests
+# antTargetArgument=" -antTarget runWithoutTestBuild "
