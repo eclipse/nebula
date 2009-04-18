1950cae52686deba2484e9b916bd41b7a8815639
rnicolas <rnicolas>
2009-04-18 10:03:09 +0000

    Added test suite

---
+++ org.eclipse.nebula.widgets.gallery.tests/src/org/eclipse/nebula/widgets/gallery/tests/AllTests.java
@@ -0,0 +1,21 @@
+package org.eclipse.nebula.widgets.gallery.tests;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(
+				"Test for org.eclipse.nebula.widgets.gallery.tests");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(GalleryVirtualBehaviorTest.class);
+		suite.addTestSuite(Bug212182Test.class);
+		suite.addTestSuite(GalleryTest.class);
+		suite.addTestSuite(Bug217988Test.class);
+		suite.addTestSuite(Bug216204Test.class);
+		//$JUnit-END$
+		return suite;
+	}
+
+}
