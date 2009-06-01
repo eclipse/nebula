943742a261baf00e84d3be4f296f9fb27c99775d
rnicolas <rnicolas>
2009-06-01 16:03:55 +0000

    Created compositetable releng

---
+++ releng/org.eclipse.nebula.widgets.compositetable.releng/compositetable_build_properties.sh
@@ -0,0 +1,8 @@
+nebulaProjectId="compositetable"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.compositetable.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="0.5.0";
+buildType="N"
+# Add this line if no tests
+antTargetArgument=" -antTarget runWithoutTestBuild "
