96a5a92c607a1d4f29d080afb58a3503d9672dbf
rnicolas <rnicolas>
2009-06-01 11:59:37 +0000

    Created collapsible buttons releng

---
+++ releng/org.eclipse.nebula.widgets.collapsiblebuttons.releng/collapsiblebuttons_build_properties.sh
@@ -0,0 +1,8 @@
+nebulaProjectId="collapsiblebuttons"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.collapsiblebuttons.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="0.5.0";
+buildType="N"
+# Add this line if no tests
+antTargetArgument=" -antTarget runWithoutTestBuild "
