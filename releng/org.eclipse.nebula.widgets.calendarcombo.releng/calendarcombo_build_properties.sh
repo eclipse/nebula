46f5e242f7c72ae1f7d1d058ec84396a4b3ce70c
rnicolas <rnicolas>
2009-06-01 15:36:33 +0000

    Added calendar combo releng

---
+++ releng/org.eclipse.nebula.widgets.calendarcombo.releng/calendarcombo_build_properties.sh
@@ -0,0 +1,8 @@
+nebulaProjectId="calendarcombo"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.calendarcombo.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="0.5.0";
+buildType="N"
+# Add this line if no tests
+antTargetArgument=" -antTarget runWithoutTestBuild "
