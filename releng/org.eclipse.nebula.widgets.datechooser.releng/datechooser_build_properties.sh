74352337c627b6505bd0f8c0c67114c090b56e04
ewuillai <ewuillai>
2009-06-17 21:29:33 +0000

    DateChooser build project initial creation

---
+++ releng/org.eclipse.nebula.widgets.datechooser.releng/datechooser_build_properties.sh
@@ -0,0 +1,8 @@
+nebulaProjectId="datechooser"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.datechooser.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="1.0.0";
+buildType="N"
+# Add this line if no tests
+# antTargetArgument=" -antTarget runWithoutTestBuild "
