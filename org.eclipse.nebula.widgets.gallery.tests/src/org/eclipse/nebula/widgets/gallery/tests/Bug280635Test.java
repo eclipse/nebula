01dafd8fc6ffb9e238fdd820ae60c17d6318e8ad
rnicolas <rnicolas>
2009-06-18 13:34:54 +0000

    RESOLVED - bug 280635: Calling dispose on GalleryItem results in infinite loop https://bugs.eclipse.org/bugs/show_bug.cgi?id=280635
    Added test case for Tree for compare.

--- org.eclipse.nebula.widgets.gallery.tests/src/org/eclipse/nebula/widgets/gallery/tests/Bug280635Test.java
+++ org.eclipse.nebula.widgets.gallery.tests/src/org/eclipse/nebula/widgets/gallery/tests/Bug280635Test.java
@@ -11,6 +11,8 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 
 public class Bug280635Test extends TestCase {
 	Display d = null;
@@ -19,7 +21,7 @@ public class Bug280635Test extends TestCase {
 
 	protected void setUp() throws Exception {
 		d = Display.getCurrent();
-		if( d == null ){
+		if (d == null) {
 			d = new Display();
 			createdDisplay = true;
 		}
@@ -28,8 +30,8 @@ public class Bug280635Test extends TestCase {
 	}
 
 	protected void tearDown() throws Exception {
-		if( createdDisplay){
-		d.dispose();
+		if (createdDisplay) {
+			d.dispose();
 		}
 		super.tearDown();
 	}
@@ -37,38 +39,76 @@ public class Bug280635Test extends TestCase {
 	public void testBug280635() {
 		final Gallery g = createGallery(SWT.V_SCROLL | SWT.VIRTUAL);
 
-		 g.addListener(SWT.SetData, new Listener() {
+		g.addListener(SWT.SetData, new Listener() {
 
-	         public void handleEvent( Event event ) {
-	            GalleryItem item = (GalleryItem)event.item;
-	            int index;
-	            if ( item.getParentItem() != null ) {
-	               index = item.getParentItem().indexOf(item);
-	               item.setItemCount(0);
-	            }
-	            else {
-	               index = g.indexOf(item);
-	               item.setItemCount(100);
-	            }
+			public void handleEvent(Event event) {
+				GalleryItem item = (GalleryItem) event.item;
+				int index;
+				if (item.getParentItem() != null) {
+					index = item.getParentItem().indexOf(item);
+					item.setItemCount(0);
+				} else {
+					index = g.indexOf(item);
+					item.setItemCount(100);
+				}
 
-	            System.out.println("setData index " + index); //$NON-NLS-1$
-	            // Your image here
-	            // item.setImage(eclipseImage);
-	            item.setText("Item " + index); //$NON-NLS-1$
-	         }
+				System.out.println("setData index " + index); //$NON-NLS-1$
+				// Your image here
+				// item.setImage(eclipseImage);
+				item.setText("Item " + index); //$NON-NLS-1$
+			}
 
-	      });
-		 
-		 
-		  g.setItemCount(1);
-		  
-		  g.getItem(0).dispose();
-		  
+		});
+
+		g.setItemCount(1);
+
+		g.getItem(0).dispose();
+
+		assertEquals(g.getItemCount(), 0);
+
 		g.dispose();
 
 	}
 
-	
+	public void testBug280635ForTree() {
+		final Tree g = createTree(SWT.V_SCROLL | SWT.VIRTUAL);
+
+		g.addListener(SWT.SetData, new Listener() {
+
+			public void handleEvent(Event event) {
+				TreeItem item = (TreeItem) event.item;
+				int index;
+				if (item.getParentItem() != null) {
+					index = item.getParentItem().indexOf(item);
+					item.setItemCount(0);
+				} else {
+					index = g.indexOf(item);
+					item.setItemCount(100);
+				}
+
+				System.out.println("setData index " + index); //$NON-NLS-1$
+				// Your image here
+				// item.setImage(eclipseImage);
+				item.setText("Item " + index); //$NON-NLS-1$
+			}
+
+		});
+
+		g.setItemCount(1);
+
+		g.getItem(0).dispose();
+
+		assertEquals(g.getItemCount(), 0);
+		g.dispose();
+
+	}
+
+	private Tree createTree(int flags) {
+		Tree tree = new Tree(s, flags);
+
+		return tree;
+	}
+
 	private Gallery createGallery(int flags) {
 		Gallery g = new Gallery(s, flags);
 
