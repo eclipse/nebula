eb3fd49f367cd53bdead783b481ce27e80bc5c9a
tschindl <tschindl>
2009-06-19 10:47:20 +0000

    fixed copyright

--- org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/grid/viewer/GridViewerSnippet7.java
+++ org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/grid/viewer/GridViewerSnippet7.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Tom Schindl and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Tom Schindl - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.swt.nebula.snippets.grid.viewer;
 
 import java.beans.BeanInfo;
