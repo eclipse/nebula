a039683100031bf1e4806e3048f04d36ce219997
rnicolas <rnicolas>
2009-06-23 09:43:39 +0000

    Renamed build properties

---
+++ releng/org.eclipse.nebula.widgets.formattedtext.releng/formatedtext_build_properties.sh
@@ -0,0 +1,8 @@
+nebulaProjectId="formattedtext"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.formattedtext.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="1.0.0";
+buildType="N"
+# Add this line if no tests
+antTargetArgument=" -antTarget runWithoutTestBuild "
