5aaa27263294a4ab4656b1a30ea185dc6a782d31
nricheto <nricheto>
2010-01-06 22:23:06 +0000

    Fixed headers

--- org.eclipse.nebula.widgets.gallery/src/org/eclipse/nebula/animation/effects/CrossFadeEffect.java
+++ org.eclipse.nebula.widgets.gallery/src/org/eclipse/nebula/animation/effects/CrossFadeEffect.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2010 Nicolas Richeton.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors :
+ *    Nicolas Richeton (nicolas.richeton@gmail.com) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.nebula.animation.effects;
 
 import org.eclipse.nebula.animation.movement.IMovement;
