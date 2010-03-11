1ad638c01f3d6edc22f1544d79ecec87f6ee3b6b
rnicolas <rnicolas>
2010-03-11 16:05:22 +0000

    Fixed releng

---
+++ releng/org.eclipse.nebula.widgets.xviewer.releng/xviewer_build_properties.sh
@@ -0,0 +1,8 @@
+nebulaProjectId="xviewer"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.xviewer.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="1.0.0";
+buildType="N"
+# Add this line if no tests
+# antTargetArgument=" -antTarget runWithoutTestBuild "
