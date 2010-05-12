9599c69577f732561c6c1048f283cad406a9b220
tschindl <tschindl>
2010-05-12 18:57:24 +0000

    Bug 312391  -  Add Support for ToolItems  * added example

--- org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/pgroup/PGroupSnippet3.java
+++ org.eclipse.swt.nebula.snippets/src/org/eclipse/swt/nebula/snippets/pgroup/PGroupSnippet3.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006 BestSolution.at and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     Tom Schindl<tom.schindl@bestsolution.at> - initial API and implementation
  *******************************************************************************/
 package org.eclipse.swt.nebula.snippets.pgroup;
 
