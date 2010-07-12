3c64b30e16a203475d8796f6a5af5a499b2b233b
ddunne <ddunne>
2010-07-12 18:37:28 +0000

    Created XViewer Test Fragment

---
+++ org.eclipse.nebula.widgets.xviewer.tests/src/org/eclipse/nebula/widgets/xviewer/tests/AllXViewerTestSuite.java
@@ -0,0 +1,17 @@
+/*
+ * Created on Jul 12, 2010
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.nebula.widgets.xviewer.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ //
+DummyTest.class, //
+})
+public class AllXViewerTestSuite {
+
+}
---
+++ org.eclipse.nebula.widgets.xviewer.tests/src/org/eclipse/nebula/widgets/xviewer/tests/DummyTest.java
@@ -0,0 +1,16 @@
+/*
+ * Created on Jul 12, 2010
+ *
+ * PLACE_YOUR_DISTRIBUTION_STATEMENT_RIGHT_HERE
+ */
+package org.eclipse.nebula.widgets.xviewer.tests;
+
+import junit.framework.Assert;
+
+public class DummyTest {
+
+	@org.junit.Test
+	public void dummyTest() {
+		Assert.assertTrue("Dummy test", true);
+	}
+}
