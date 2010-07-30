a1f5ea5d23682b385b7d51c9294bca7b1fa9d364
wjongman <wjongman>
2010-07-30 20:44:14 +0000

    Initital commit

---
+++ releng/org.eclipse.nebula.widgets.oscilloscope.releng/oscilloscope_build_properties.sh
@@ -0,0 +1,8 @@
+nebulaProjectId="gallery"
+relengProjectPath="org.eclipse.swt.nebula/releng/org.eclipse.nebula.widgets.oscilloscope.releng"
+projectid="technology.nebula.$nebulaProjectId"
+sub="nebula.$nebulaProjectId"
+version="1.0.0";
+buildType="N"
+# Add this line if no tests
+# antTargetArgument=" -antTarget runWithoutTestBuild "
