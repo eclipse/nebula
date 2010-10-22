3d182f10c83a13a71b4b41a973bc84c08660ab97
ddunne <ddunne>
2010-10-22 20:00:25 +0000

    fixed variable name

--- org.eclipse.nebula.widgets.xviewer/src/org/eclipse/nebula/widgets/xviewer/example/MyXViewerTest.java
+++ org.eclipse.nebula.widgets.xviewer/src/org/eclipse/nebula/widgets/xviewer/example/MyXViewerTest.java
@@ -33,7 +33,7 @@ import org.eclipse.swt.widgets.Shell;
  * @author Donald G. Dunne
  */
 public class MyXViewerTest {
-   static MyXViewer xViewerTest;
+   static MyXViewer myXviewer;
 
    public static void main(String[] args) {
       Display Display_1 = Display.getDefault();
@@ -52,20 +52,20 @@ public class MyXViewerTest {
             for (int x = 0; x < 1; x++) {
                tasks.addAll(getTestTasks());
             }
-            xViewerTest.setInput(tasks);
+            myXviewer.setInput(tasks);
          }
       });
 
-      xViewerTest = new MyXViewer(Shell_1, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
-      xViewerTest.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
-      xViewerTest.setContentProvider(new MyXViewerContentProvider());
-      xViewerTest.setLabelProvider(new MyXViewerLabelProvider(xViewerTest));
+      myXviewer = new MyXViewer(Shell_1, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+      myXviewer.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
+      myXviewer.setContentProvider(new MyXViewerContentProvider());
+      myXviewer.setLabelProvider(new MyXViewerLabelProvider(myXviewer));
 
       List<Object> tasks = new ArrayList<Object>();
       for (int x = 0; x < 1; x++) {
          tasks.addAll(getTestTasks());
       }
-      xViewerTest.setInput(tasks);
+      myXviewer.setInput(tasks);
       Shell_1.open();
       while (!Shell_1.isDisposed()) {
          if (!Display_1.readAndDispatch()) {
@@ -87,39 +87,39 @@ public class MyXViewerTest {
       List<ISomeTask> tasks = new ArrayList<ISomeTask>();
       tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Backup, getDate(), "org.eclipse.osee.test1", "10:03",
          "run to test this", "Suite A", "mark@eclipse.com", 50));
-      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test2",
-         "9:22", "run to test that", "Suite B", "john@eclipse.com", 0));
+      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test2", "9:22",
+         "run to test that", "Suite B", "john@eclipse.com", 0));
       tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Backup, getDate(), "org.eclipse.osee.test4", "8:23",
          "in this world", "Suite A", "john@eclipse.com", 50));
       tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Backup, getDate(), "org.eclipse.osee.test3", "23:01",
          "now is the time", "Suite B", "mike@eclipse.com", 30));
-      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Db_Health, getDate(), "org.eclipse.osee.test5",
-         "7:32", "may be never", "Suite A", "steve@eclipse.com", 10));
-      tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test14",
-         "6:11", "", "Suite A", "steve@eclipse.com", 95));
+      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Db_Health, getDate(), "org.eclipse.osee.test5", "7:32",
+         "may be never", "Suite A", "steve@eclipse.com", 10));
+      tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test14", "6:11", "",
+         "Suite A", "steve@eclipse.com", 95));
       tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Backup, getDate(), "org.eclipse.osee.test6", "5:13",
          "run to test this", "Suite B", "john@eclipse.com", 80));
-      tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Db_Health, getDate(), "org.eclipse.osee.test12", "23:15",
-         "", "Suite A", "mike@eclipse.com", 90));
+      tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Db_Health, getDate(), "org.eclipse.osee.test12", "23:15", "",
+         "Suite A", "mike@eclipse.com", 90));
       tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Backup, getDate(), "org.eclipse.osee.test13", "4:01",
          "run to test this", "Suite B", "steve@eclipse.com", 100));
-      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test11",
-         "3:16", "run to test this", "Suite A", "steve@eclipse.com", 53));
+      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test11", "3:16",
+         "run to test this", "Suite A", "steve@eclipse.com", 53));
       tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Backup, getDate(), "org.eclipse.osee.test10", "5:01",
          "run to test this", "Suite C", "mike@eclipse.com", 0));
-      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test9",
-         "4:27", "run to test this", "Suite C", "steve@eclipse.com", 90));
-      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Regression, getDate(), "org.eclipse.osee.test7",
-         "2:37", "run to test this", "Suite C", "john@eclipse.com", 20));
+      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Data_Exchange, getDate(), "org.eclipse.osee.test9", "4:27",
+         "run to test this", "Suite C", "steve@eclipse.com", 90));
+      tasks.add(new SomeTask(RunDb.Production_Db, TaskType.Regression, getDate(), "org.eclipse.osee.test7", "2:37",
+         "run to test this", "Suite C", "john@eclipse.com", 20));
       int num = 10;
       for (String str : Arrays.asList("Now", "Cat", "Dog", "Tree", "Bike", "Sun", "Moon", "Grass", "Can", "Car",
          "Truck", "Block", "Earth", "Mars", "Venus", "Requirements visualization", "Requirements management",
          "Feature management", "Modeling", "Design", "Project Management", "Change management",
          "Configuration Management", "Software Information Management", "Build management", "Testing",
          "Release Management", "Software Deployment", "Issue management", "Monitoring and reporting", "Workflow")) {
-         tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Db_Health, getDate(), "org.eclipse.osee." + str,
-            "24:" + num++, str + " will run to test this", "Suite C" + num++,
-            str.toLowerCase().replaceAll(" ", ".") + "@eclipse.com", 20));
+         tasks.add(new SomeTask(RunDb.Test_Db, TaskType.Db_Health, getDate(), "org.eclipse.osee." + str, "24:" + num++,
+            str + " will run to test this", "Suite C" + num++, str.toLowerCase().replaceAll(" ", ".") + "@eclipse.com",
+            20));
       }
       return tasks;
    }
