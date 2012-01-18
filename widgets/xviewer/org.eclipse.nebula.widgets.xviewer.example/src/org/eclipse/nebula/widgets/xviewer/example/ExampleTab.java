0b74b5bac15cccf3fb27f55be14eae4122fb8c7a
Wim Jongman <wim.jongman@remainsoftware.com>
2012-01-18 17:03:39 +0100

    bug 368889: [Examples] Nebula Examples view problems https://bugs.eclipse.org/bugs/show_bug.cgi?id=368889

---
+++ widgets/xviewer/org.eclipse.nebula.widgets.xviewer.example/src/org/eclipse/nebula/widgets/xviewer/example/ExampleTab.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Remain Software
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Wim jongman <wim.jongman@remainsoftware.com> - creation
+ *******************************************************************************/
+
+package org.eclipse.nebula.widgets.xviewer.example;
+
+import org.eclipse.nebula.examples.AbstractExampleTab;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+
+public class ExampleTab extends AbstractExampleTab {
+
+	/**
+	 * @wbp.parser.entryPoint
+	 */
+	public Control createControl(Composite parent) {
+		int style = SWT.None;
+		Button button = new Button(parent, style);
+		button.setText("Run XViewer Example");
+		button.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				new MyXViewerTest().main(new String[0]);
+			}
+		});
+
+		return button;
+	}
+
+	public String[] createLinks() {
+		return null;
+	}
+
+	public void createParameters(Composite parent) {
+
+	}
+
+}
