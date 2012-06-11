8f07febc3d1b6b4ae831c57c1c484fe9c10d3f5d
Wim Jongman <wim.jongman@remainsoftware.com>
2012-06-12 00:29:40 +0200

    Revert "bug 382027: Cannot get to events during undo/redo cycle  https://bugs.eclipse.org/bugs/show_bug.cgi?id=382027"
    This reverts commit 7f70422e7165447374e5c3f99dfdc04ef820e7e0.

--- widgets/ganttchart/org.eclipse.nebula.widgets.ganttchart/src/org/eclipse/nebula/widgets/ganttchart/undoredo/commands/ClusteredCommand.java
+++ widgets/ganttchart/org.eclipse.nebula.widgets.ganttchart/src/org/eclipse/nebula/widgets/ganttchart/undoredo/commands/ClusteredCommand.java
@@ -15,81 +15,86 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.nebula.widgets.ganttchart.GanttEvent;
+
 /**
- * One command to handle many sub-commands, such as a multi-drag/drop etc. All commands inside a clustered command will be Undone/Redone at the same time.
+ * One command to handle many sub-commands, such as a multi-drag/drop etc. All
+ * commands inside a clustered command will be Undone/Redone at the same time.
  * 
  * @author cre
- *
+ * 
  */
 public class ClusteredCommand extends AbstractUndoRedoCommand {
 
-    private final List _commands;
-    
-    /**
-     * Creates a new Clustered Command.
-     */
-    public ClusteredCommand() {
-        super();
-        _commands = new ArrayList();
-    }
-    
-    /**
-     * Creates a new Clustered Command witha list of pre-set commands.
-     */
-    public ClusteredCommand(final List commands) {
-        super();
-        _commands = commands;
-    }
-    
-    /**
-     * Adds a new command to the cluster.
-     * 
-     * @param command Command to add
-     */
-    public void addCommand(final IUndoRedoCommand command) {
-    	if (command == null) {
-    		return;
-    	}
-    	
-        _commands.add(command);
-    }
-    
-    /**
-     * Removes a command from the cluster.
-     * 
-     * @param command Command to remove
-     */
-    public void removeCommand(final IUndoRedoCommand command) {
-        _commands.remove(command);
-    }
-    
-    /**
-     * Returns the number of commands that are inside the cluster.
-     * 
-     * @return Number of commands
-     */
-    public int size() {
-        return _commands.size();
-    }
-    
-    public void dispose() {
-        for (int i = 0; i < _commands.size(); i++) {
-            ((IUndoRedoCommand)_commands.get(i)).dispose();
-        }
-    }
+	private final List _commands;
 
-    public void redo() {
-        for (int i = 0; i < _commands.size(); i++) {
-            ((IUndoRedoCommand)_commands.get(i)).redo();
-        }
-    }
+	/**
+	 * Creates a new Clustered Command.
+	 */
+	public ClusteredCommand() {
+		super();
+		_commands = new ArrayList();
+	}
 
-    public void undo() {
-        for (int i = 0; i < _commands.size(); i++) {
-            ((IUndoRedoCommand)_commands.get(i)).undo();
-        }
-    }
-    
+	/**
+	 * Creates a new Clustered Command witha list of pre-set commands.
+	 */
+	public ClusteredCommand(final List commands) {
+		super();
+		_commands = commands;
+	}
+
+	/**
+	 * Adds a new command to the cluster.
+	 * 
+	 * @param command
+	 *            Command to add
+	 */
+	public void addCommand(final IUndoRedoCommand command) {
+		if (command == null) {
+			return;
+		}
+
+		_commands.add(command);
+	}
+
+	/**
+	 * Removes a command from the cluster.
+	 * 
+	 * @param command
+	 *            Command to remove
+	 */
+	public void removeCommand(final IUndoRedoCommand command) {
+		_commands.remove(command);
+	}
+
+	/**
+	 * Returns the number of commands that are inside the cluster.
+	 * 
+	 * @return Number of commands
+	 */
+	public int size() {
+		return _commands.size();
+	}
+
+	public void dispose() {
+		for (int i = 0; i < _commands.size(); i++) {
+			((IUndoRedoCommand) _commands.get(i)).dispose();
+		}
+	}
+
+	public void redo() {
+		for (int i = 0; i < _commands.size(); i++) {
+			((IUndoRedoCommand) _commands.get(i)).redo();
+		}
+	}
+
+	public void undo() {
+		for (int i = 0; i < _commands.size(); i++) {
+			((IUndoRedoCommand) _commands.get(i)).undo();
+		}
+	}
+
 	/**
 	 * Return the individual commands that are clustered in this command.
 	 * 
