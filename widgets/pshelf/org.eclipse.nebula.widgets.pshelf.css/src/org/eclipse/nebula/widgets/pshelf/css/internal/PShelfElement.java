71411dd7b7136dc9164b36fceef414070d3e4886
Tom Schindl <tom.schindl@bestsolution.at>
2013-04-08 14:40:53 +0200

    added missing copyright, ...

--- widgets/pshelf/org.eclipse.nebula.widgets.pshelf.css/src/org/eclipse/nebula/widgets/pshelf/css/internal/PShelfElement.java
+++ widgets/pshelf/org.eclipse.nebula.widgets.pshelf.css/src/org/eclipse/nebula/widgets/pshelf/css/internal/PShelfElement.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Tom Schindl. All rights reserved. This program and the
+ * accompanying materials are made available under the terms of the Eclipse
+ * Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html 
+ * 
+ * Contributors: Tom Schindl <tom.schindl@bestsolution.at> - initial API and implementation
+ ******************************************************************************/
 package org.eclipse.nebula.widgets.pshelf.css.internal;
 
 import org.eclipse.e4.ui.css.core.engine.CSSEngine;
