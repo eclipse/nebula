775c6ab4e255e765ccdd63aedb1f97aafe9cc063
Wim Jongman <wim.jongman@remainsoftware.com>
2014-04-13 14:49:35 +0200

    xxx
    Change-Id: I19ccfbefb0625ba787c52102e688ec098a55b1ab
    Signed-off-by: Wim Jongman <wim.jongman@remainsoftware.com>

--- widgets/grid/org.eclipse.nebula.jface.gridviewer/src/org/eclipse/nebula/jface/gridviewer/GridViewerRow.java
+++ widgets/grid/org.eclipse.nebula.jface.gridviewer/src/org/eclipse/nebula/jface/gridviewer/GridViewerRow.java
@@ -32,7 +32,7 @@ public class GridViewerRow extends ViewerRow
     private GridItem item;
 
     /**
-     * Create a new instance of the receiver.
+     * Create a new instance of the receiver. Test
      *
      * @param item GridItem source.
      */
