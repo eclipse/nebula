519d4c402ca5b1686f62f867777346908d96bdab
Baha El Kassaby <baha.elkassaby@gmail.com>
2017-05-25 22:30:27 +0200

    Bug 517241 - Correctly set axis range
    Change-Id: I0fc4041dbad03e1c614b66c16962cf391865bae6
    Signed-off-by: Baha El Kassaby <baha.elkassaby@gmail.com>
    Also-by: Peter Chang <peter.chang@diamond.ac.uk>

--- widgets/visualization/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/internal/xygraph/toolbar/AxisConfigPage.java
+++ widgets/visualization/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/internal/xygraph/toolbar/AxisConfigPage.java
@@ -403,6 +403,7 @@ public class AxisConfigPage {
 			primaryButton.setEnabled(false);
 		logButton.setSelection(axis.isLogScaleEnabled());
 		autoScaleButton.setSelection(axis.isAutoScale());
+		Range r = axis.getLocalRange();
 		if (autoScaleButton.getSelection()) {
 			maxOrAutoScaleLabel.setText("Auto Scale Threshold(%)");
 			maxOrAutoScaleThrText.setRange(new Range(0, 100));
@@ -412,12 +413,12 @@ public class AxisConfigPage {
 		} else {
 			maxOrAutoScaleLabel.setText("Maximum");
 			maxOrAutoScaleThrText.setRange(null);
-			maxOrAutoScaleThrText.getText().setText(String.valueOf(axis.getRange().getUpper()));
+			maxOrAutoScaleThrText.getText().setText(String.valueOf(r.getUpper()));
 			minLabel.setVisible(true);
 			minText.getText().setVisible(true);
 		}
 
-		minText.getText().setText(String.valueOf(axis.getRange().getLower()));
+		minText.getText().setText(String.valueOf(r.getLower()));
 		dateEnabledButton.setSelection(axis.isDateEnabled());
 		autoFormat.setSelection(axis.isAutoFormat());
 		formatLabel.setText(dateEnabledButton.getSelection() ? "Time Format: " : "Numeric Format: ");
