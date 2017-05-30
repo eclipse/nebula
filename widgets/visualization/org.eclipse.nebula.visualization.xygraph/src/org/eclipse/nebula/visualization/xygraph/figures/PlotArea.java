c584c64aac506799cda0d2618ca825991fd663d4
Baha El Kassaby <baha.elkassaby@gmail.com>
2017-05-30 11:40:34 +0200

    Bug 517412 - add a getter for xyGraph
    Change-Id: I682d8735b0d2723e3bec506f51f8860b1002ba50
    Signed-off-by: Baha El Kassaby <baha.elkassaby@gmail.com>

--- widgets/visualization/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/figures/PlotArea.java
+++ widgets/visualization/org.eclipse.nebula.visualization.xygraph/src/org/eclipse/nebula/visualization/xygraph/figures/PlotArea.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010 Oak Ridge National Laboratory.
+ * Copyright (c) 2010, 2017 Oak Ridge National Laboratory and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -301,6 +301,13 @@ public class PlotArea extends Figure {
 	 */
 	public ZoomType getZoomType() {
 		return zoomType;
+	}
+
+	/**
+	 * @return xyGraph
+	 */
+	public IXYGraph getXYGraph() {
+		return xyGraph;
 	}
 
 	/**
