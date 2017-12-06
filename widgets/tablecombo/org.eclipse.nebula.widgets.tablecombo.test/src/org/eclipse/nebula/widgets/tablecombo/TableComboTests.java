618dccc521bc8d8bb874ec43549057b4b623a5f8
scholz <matthias.paul.scholz@gmail.com>
2017-12-06 14:38:53 +0100

    Bug 514731 TableCombo sends a selection event on each opening of the drop down box
    Additional tests for TableCombo and TableComboViewer.
    
    Change-Id: I7e45d50869844f3546c04ed7d51dc81db6306764
    Signed-off-by: Matthias Paul Scholz <matthias.paul.scholz@gmail.com>

--- widgets/tablecombo/org.eclipse.nebula.widgets.tablecombo.test/src/org/eclipse/nebula/widgets/tablecombo/TableComboTests.java
+++ widgets/tablecombo/org.eclipse.nebula.widgets.tablecombo.test/src/org/eclipse/nebula/widgets/tablecombo/TableComboTests.java
@@ -12,6 +12,7 @@ import static org.junit.Assert.assertEquals;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TableItem;
@@ -32,8 +33,8 @@ public class TableComboTests {
   }
 
   /**
-   * Ensures that the elements set as items for the the {@link TableCombo} are
-   * correctly displayed in the table widget.
+   * Ensures that the items in the {@link TableCombo} are correctly displayed in
+   * the table widget.
    */
   @Test
   public void testDisplayOfItems() {
@@ -42,36 +43,108 @@ public class TableComboTests {
      * GIVEN a TableCombo with one column and a number of TableItems assigned to
      * the TableCombo.
      */
-    final TableCombo tableCombo = new TableCombo(shell, SWT.NONE);
-    tableCombo.setLayoutData(new GridData(SWT.FILL, SWT.DEFAULT, true, false));
-
-    tableCombo.defineColumns(new String[] {
-        "Id"
-    });
-
-    final int numberOfTableItems = 5;
-    for (int i = 0; i < numberOfTableItems; i++) {
-      createTableItem(tableCombo, "item " + i);
-    }
+    createTableCombo(shell, 1, 5);
 
     /*
      * WHEN the TableCombo gets displayed
      */
     shell.open();
-    consumeEvents();
 
     /*
      * THEN the number of rows displayed in the table widget of the TableCombo
-     * must match the numer of TableItems.
+     * must match the number of TableItems.
      */
     final SWTBot shellBot = new SWTBot(shell);
     final TableCombo tableComboWidget = shellBot
         .widget(WidgetMatcherFactory.widgetOfType(TableCombo.class));
     final SWTBotTable tableBot = new SWTBotTable(tableComboWidget.getTable());
 
-    assertEquals("Expected the number of items in the table widget of the TableCombo to be the same as the number of TableItems added to the TableCombo", numberOfTableItems, tableBot
+    assertEquals("Expected the number of items in the table widget of the TableCombo to be the same as the number of TableItems added to the TableCombo", 5, tableBot
         .rowCount());
 
+  }
+
+  /**
+   * Ensures that the columns defined for a {@link TableCombo} are correctly
+   * displayed in the table widget.
+   */
+  @Test
+  public void testDisplayOfColumns() {
+
+    /*
+     * GIVEN a TableCombo with a number of columns and a number of TableItems
+     * assigned to the TableCombo.
+     */
+    createTableCombo(shell, 3, 5);
+
+    /*
+     * WHEN the TableCombo gets displayed
+     */
+    shell.open();
+
+    /*
+     * THEN the number of columns displayed in the table widget of the
+     * TableCombo must match the configured number
+     */
+    final SWTBot shellBot = new SWTBot(shell);
+    final TableCombo tableComboWidget = shellBot
+        .widget(WidgetMatcherFactory.widgetOfType(TableCombo.class));
+    final SWTBotTable tableBot = new SWTBotTable(tableComboWidget.getTable());
+
+    assertEquals("Expected the number of columns in the table widget of the TableCombo to be the same as the number of columns configured in the TableCombo", 3, tableBot
+        .columnCount());
+
+  }
+
+  /**
+   * Ensures that the text displayed in tableCombo text widget corresponds to
+   * the item selected in the tableCombo.
+   */
+  @Test
+  public void testSelectionOfSingleItem() {
+
+    /*
+     * GIVEN a TableCombo with one column and a number of TableItems assigned to
+     * the TableCombo.
+     */
+    createTableCombo(shell, 1, 5);
+
+    /*
+     * WHEN the TableCombo gets displayed and a single item is selected in the
+     * table combo
+     */
+    shell.open();
+
+    final SWTBot shellBot = new SWTBot(shell);
+    final TableCombo tableComboWidget = shellBot
+        .widget(WidgetMatcherFactory.widgetOfType(TableCombo.class));
+    tableComboWidget.select(0);
+
+    /*
+     * THEN the text displayed in tableCombo text widget must correspond to the
+     * item selected in the tableCombo.
+     */
+    assertEquals("Expected the text displayed in the tableCombo text widget to correspond to the item selected in the tableCombo", "Item 0", tableComboWidget
+        .getText());
+
+  }
+
+  private TableCombo createTableCombo(final Composite parent, final int noOfColumns, final int noOfItems) {
+    final TableCombo result = new TableCombo(parent, SWT.NONE);
+    result.setLayoutData(new GridData(SWT.FILL, SWT.DEFAULT, true, false));
+
+    // create columns
+    final String[] columns = new String[noOfColumns];
+    for (int i = 0; i < noOfColumns; i++) {
+      columns[i] = "Column " + i;
+    }
+    result.defineColumns(columns);
+
+    // create table items
+    for (int i = 0; i < noOfItems; i++) {
+      createTableItem(result, "Item " + i);
+    }
+    return result;
   }
 
   private void createTableItem(final TableCombo tableCombo, final String text) {
@@ -79,8 +152,4 @@ public class TableComboTests {
     tableItem.setText(text);
   }
 
-  private void consumeEvents() {
-    while (shell.getDisplay().readAndDispatch()) {
-    }
-  }
 }
---
+++ widgets/tablecombo/org.eclipse.nebula.widgets.tablecombo.test/src/org/eclipse/nebula/widgets/tablecombo/TableComboViewerTests.java
@@ -0,0 +1,159 @@
+/*******************************************************************************
+ * Copyright (c) 2017 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+******************************************************************************/
+package org.eclipse.nebula.widgets.tablecombo;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.nebula.jface.tablecomboviewer.TableComboViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.matchers.WidgetMatcherFactory;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTable;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TableComboViewerTests {
+
+  private Shell shell;
+
+  @Before
+  public final void setupTableComboViewerTests() {
+    shell = new Shell(Display.getDefault());
+    shell.setLayout(new GridLayout());
+  }
+
+  /**
+   * Ensures that the items set as input to the {@link TableComboViewer} are
+   * correctly displayed in the table widget.
+   */
+  @Test
+  public void testDisplayOfItems() {
+
+    /*
+     * GIVEN a TableComboViewer with two columns and a number of items set as
+     * input of the TableComboViewer.
+     */
+    createTableComboViewer(shell, 5);
+
+    /*
+     * WHEN the TableComboViewer gets displayed
+     */
+    shell.open();
+
+    /*
+     * THEN the number of rows displayed in the table widget of the
+     * TableComboViewer must match the number of items.
+     */
+    final SWTBot shellBot = new SWTBot(shell);
+    final TableCombo tableComboWidget = shellBot
+        .widget(WidgetMatcherFactory.widgetOfType(TableCombo.class));
+    final SWTBotTable tableBot = new SWTBotTable(tableComboWidget.getTable());
+
+    assertEquals("Expected the number of items in the table widget of the TableCombo to be the same as the number of items added to the TableComboViewer", 5, tableBot
+        .rowCount());
+
+  }
+
+  /**
+   * Ensures that the properties of the items for the {@link TableComboViewer}
+   * are correctly displayed in the cells of the table widget.
+   */
+  @Test
+  public void testDisplayOfCellValues() {
+
+    /*
+     * GIVEN a TableComboViewer with two columns and a number of items set as
+     * input of the TableComboViewer.
+     */
+    createTableComboViewer(shell, 5);
+
+    /*
+     * WHEN the TableComboViewer gets displayed
+     */
+    shell.open();
+
+    /*
+     * THEN the number of columns displayed in the table widget of the
+     * TableComboViewer must match the number of columns defined for the
+     * TableComboViewer and the cell values must reflect the according property
+     * values of the item in the row at hand.
+     */
+    final SWTBot shellBot = new SWTBot(shell);
+    final TableCombo tableComboWidget = shellBot
+        .widget(WidgetMatcherFactory.widgetOfType(TableCombo.class));
+    final SWTBotTable tableBot = new SWTBotTable(tableComboWidget.getTable());
+
+    assertEquals("Expected the number of columns in the table widget of the TableComboViewer to be the one as defined for the TableComboViewer", 2, tableBot
+        .columnCount());
+    assertEquals("Expected the value in the cell [0,1] in the table widget of the TableComboViewer to correspond to the description of first item set as input for the TableComboViewer", "Description 0", tableBot
+        .cell(0, 1));
+    assertEquals("Expected the value in the cell [1,0] in the table widget of the TableComboViewer to correspond to the name of second item set as input for the TableComboViewer", "Item 1", tableBot
+        .cell(1, 0));
+
+  }
+
+  private TableComboViewer createTableComboViewer(final Composite parent, final int noOfItems) {
+    final TableComboViewer result = new TableComboViewer(parent, SWT.NONE);
+    result.getTableCombo()
+        .setLayoutData(new GridData(SWT.FILL, SWT.DEFAULT, true, false));
+
+    result.setContentProvider(ArrayContentProvider.getInstance());
+    result.setLabelProvider(new TestLabelProvider());
+
+    // create columns
+    final String[] columns = new String[] {
+        "Name", "Description"
+    };
+    result.getTableCombo().defineColumns(columns);
+
+    // set input data
+    final TestItem[] testItems = new TestItem[noOfItems];
+    for (int i = 0; i < noOfItems; i++) {
+      testItems[i] = new TestItem("Item " + i, "Description " + i);
+    }
+    result.setInput(testItems);
+
+    return result;
+
+  }
+
+  private class TestLabelProvider extends LabelProvider
+      implements ITableLabelProvider {
+
+    @Override
+    public String getColumnText(final Object element, final int columnIndex) {
+      final TestItem item = (TestItem) element;
+      return columnIndex == 0 ? item.name : item.description;
+    }
+
+    @Override
+    public Image getColumnImage(final Object element, final int columnIndex) {
+      return null;
+    }
+  }
+
+  private class TestItem {
+    String name;
+    String description;
+
+    TestItem(final String name, final String description) {
+      this.name = name;
+      this.description = description;
+    }
+
+  }
+}
