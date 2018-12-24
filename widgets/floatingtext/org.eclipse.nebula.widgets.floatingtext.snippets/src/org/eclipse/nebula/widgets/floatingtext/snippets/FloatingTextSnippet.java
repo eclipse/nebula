8c41d9cb2c754c19a2dfb7b7e3f7f88ca3711a91
Wim Jongman <wim.jongman@remainsoftware.com>
2018-12-24 12:14:03 +0100

    Update FloatingTextSnippet.java

--- widgets/floatingtext/org.eclipse.nebula.widgets.floatingtext.snippets/src/org/eclipse/nebula/widgets/floatingtext/snippets/FloatingTextSnippet.java
+++ widgets/floatingtext/org.eclipse.nebula.widgets.floatingtext.snippets/src/org/eclipse/nebula/widgets/floatingtext/snippets/FloatingTextSnippet.java
@@ -1,8 +1,8 @@
 /*******************************************************************************
- * Copyright (c) 2011 Laurent CARON. All rights reserved. This program and the
- * accompanying materials are made available under the terms of the Eclipse
- * Public License v1.0 which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2018 Wim Jongman and others 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors: Laurent CARON (laurent.caron@gmail.com) - initial API and
  * implementation
@@ -22,7 +22,7 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Shell;
 
 /**
- * A simple snipper for the PromptSupport utilities
+ * A simple snippet for the FloatingText
  *
  */
 public class FloatingTextSnippet {
