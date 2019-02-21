d0ec66f626cfb102f8d01b7258261e90c968afbd
Dirk Fauth <dirk.fauth@googlemail.com>
2019-02-21 18:26:58 +0100

    Bug 543044 - locale error
    Signed-off-by: Dirk Fauth <dirk.fauth@googlemail.com>

--- widgets/richtext/org.eclipse.nebula.widgets.richtext/src/org/eclipse/nebula/widgets/richtext/RichTextEditor.java
+++ widgets/richtext/org.eclipse.nebula.widgets.richtext/src/org/eclipse/nebula/widgets/richtext/RichTextEditor.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2015, 2016 CEA LIST.
+ * Copyright (c) 2015, 2019 CEA LIST.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -12,32 +12,17 @@
  *****************************************************************************/
 package org.eclipse.nebula.widgets.richtext;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URL;
-import java.net.URLDecoder;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.StandardCopyOption;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.jface.bindings.keys.SWTKeySupport;
+import org.eclipse.nebula.widgets.richtext.painter.ResourceHelper;
 import org.eclipse.nebula.widgets.richtext.toolbar.JavaCallbackListener;
 import org.eclipse.nebula.widgets.richtext.toolbar.ToolbarButton;
 import org.eclipse.swt.SWT;
@@ -555,7 +540,10 @@ public class RichTextEditor extends Composite {
 	 * @since 1.1
 	 */
 	public void setLanguage(Locale locale, boolean update) {
-		setLanguage(locale.getLanguage(), update);
+		this.editorConfig.setLanguage(locale);
+		if (update) {
+			updateEditor();
+		}
 	}
 
 	/**
@@ -1422,113 +1410,12 @@ public class RichTextEditor extends Composite {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
-		else if (templateURL.toString().startsWith("jar")) {
+		} else if (templateURL.toString().startsWith("jar")) {
 			BusyIndicator.showWhile(Display.getDefault(), new Runnable() {
 
 				@Override
 				public void run() {
-					URL jarURL = RichTextEditor.class.getProtectionDomain().getCodeSource().getLocation();
-					File jarFileReference = null;
-					if (jarURL.getProtocol().equals("file")) {
-						try {
-							String decodedPath = URLDecoder.decode(jarURL.getPath(), "UTF-8");
-							jarFileReference = new File(decodedPath);
-						} catch (UnsupportedEncodingException e) {
-							e.printStackTrace();
-						}
-					}
-					else {
-						// temporary download of jar file
-						// necessary to be able to unzip the resources
-						try {
-							final Path jar = Files.createTempFile("richtext", ".jar");
-							Files.copy(jarURL.openStream(), jar, StandardCopyOption.REPLACE_EXISTING);
-							jarFileReference = jar.toFile();
-
-							// delete the temporary file
-							Runtime.getRuntime().addShutdownHook(new Thread() {
-								@Override
-								public void run() {
-									try {
-										Files.delete(jar);
-									} catch (IOException e) {
-										e.printStackTrace();
-									}
-								}
-							});
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-
-					if (jarFileReference != null) {
-						try (JarFile jarFile = new JarFile(jarFileReference)) {
-							String unpackDirectory = System.getProperty(JAR_UNPACK_LOCATION_PROPERTY);
-							// create the directory to unzip to
-							final java.nio.file.Path tempDir = (unpackDirectory == null)
-									? Files.createTempDirectory("richtext") : Files.createDirectories(Paths.get(unpackDirectory));
-
-							// only register the hook to delete the temp directory after shutdown if
-							// a temporary directory was used
-							if (unpackDirectory == null) {
-								Runtime.getRuntime().addShutdownHook(new Thread() {
-									@Override
-									public void run() {
-										try {
-											Files.walkFileTree(tempDir, new SimpleFileVisitor<Path>() {
-												@Override
-												public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-													Files.delete(file);
-													return FileVisitResult.CONTINUE;
-												}
-
-												@Override
-												public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-													Files.delete(dir);
-													return FileVisitResult.CONTINUE;
-												}
-
-											});
-										} catch (IOException e) {
-											e.printStackTrace();
-										}
-									}
-								});
-							}
-
-							Enumeration<JarEntry> entries = jarFile.entries();
-							while (entries.hasMoreElements()) {
-								JarEntry entry = entries.nextElement();
-								String name = entry.getName();
-								if (name.startsWith("org/eclipse/nebula/widgets/richtext/resources")) {
-									File file = new File(tempDir.toAbsolutePath() + File.separator + name);
-									if (!file.exists()) {
-										if (entry.isDirectory()) {
-											file.mkdirs();
-										}
-										else {
-											try (InputStream is = jarFile.getInputStream(entry);
-													OutputStream os = new FileOutputStream(file)) {
-												while (is.available() > 0) {
-													os.write(is.read());
-												}
-											}
-										}
-									}
-
-									// found the template.html in the jar entries, so remember the
-									// URL for further usage
-									if (name.endsWith("template.html")) {
-										templateURL = file.toURI().toURL();
-									}
-								}
-							}
-
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
+					templateURL = ResourceHelper.getRichTextResource("template.html");
 				}
 			});
 		}
