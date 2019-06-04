7e1f6fb58e8a9f156aa17a5a3562f738656c3588
uyong <gongguangyong@live.cn>
2019-06-04 13:50:02 +0800

    Update TextUtils.java
    when text is large, continually substring opreration will produce much temporary string, then occur gc continually, cause app be at a standstill and loss of response.
    
    Signed-off-by: uyong gongguangyong@live.cn

--- widgets/grid/org.eclipse.nebula.widgets.grid/src/org/eclipse/nebula/widgets/grid/internal/TextUtils.java
+++ widgets/grid/org.eclipse.nebula.widgets.grid/src/org/eclipse/nebula/widgets/grid/internal/TextUtils.java
@@ -38,10 +38,54 @@ public class TextUtils
      * @param t text to modify.
      * @param width Pixels to display.
      * @return shortened string that fits in area specified.
+     * @deprecated when text is large, performance is poor, possible occur OOM exception. suggest use {@link #getShortStr(GC, String, int)}
      */
+    @Deprecated
     public static String getShortText(GC gc, String t, int width)
     {
-        return getShortString(gc, t, width);
+        if (t == null)
+        {
+            return null;
+        }
+
+        if (t.equals(""))
+        {
+            return "";
+        }
+
+        if (width >= gc.textExtent(t).x)
+        {
+            return t;
+        }
+
+        int w = gc.textExtent("...").x;
+        String text = t;
+        int l = text.length();
+        int pivot = l / 2;
+        int s = pivot;
+        int e = pivot + 1;
+
+        while (s >= 0 && e < l)
+        {
+            String s1 = text.substring(0, s);
+            String s2 = text.substring(e, l);
+            int l1 = gc.textExtent(s1).x;
+            int l2 = gc.textExtent(s2).x;
+            if (l1 + w + l2 < width)
+            {
+                text = s1 + "..." + s2;
+                break;
+            }
+            s--;
+            e++;
+        }
+
+        if (s == 0 || e == l)
+        {
+            text = text.substring(0, 1) + "..." + text.substring(l - 1, l);
+        }
+
+        return text;
     }
     
     /**
@@ -54,8 +98,69 @@ public class TextUtils
      * @param t text to modify.
      * @param width Pixels to display.
      * @return shortened string that fits in area specified.
+     * @deprecated when text is large, performance is poor, possible occur OOM exception. suggest use {@link #getShortStr(GC, String, int)}
      */
+    @Deprecated
     public static String getShortString(GC gc, String t, int width)
+    {
+        if (t == null)
+        {
+            return null;
+        }
+
+        if (t.equals(""))
+        {
+            return "";
+        }
+
+        int textWidth = gc.stringExtent(t).x;
+	if (width >= textWidth)
+        {
+            return t;
+        }
+	String text = t;
+	int l = text.length();
+	int w = gc.stringExtent("...").x;
+	double midChar = (double)textWidth / l;
+	int pivot = (int) ((width / midChar) / 2);
+        int s = pivot;
+        int e = l - pivot + 1;
+
+        while (s >= 0 && e < l)
+        {
+            String s1 = text.substring(0, s);
+            String s2 = text.substring(e, l);
+            int l1 = gc.stringExtent(s1).x;
+            int l2 = gc.stringExtent(s2).x;
+            if (l1 + w + l2 < width)
+            {
+                text = s1 + "..." + s2;
+                break;
+            }
+            s--;
+            e++;
+        }
+
+        if (s == 0 || e == l)
+        {
+            text = text.substring(0, 1) + "..." + text.substring(l - 1, l);
+        }
+
+        return text;
+    }
+    
+    /**
+     * Shortens a supplied string so that it fits within the area specified by
+     * the width argument. Strings that have been shorted have an "..." attached
+     * to the end of the string. The width is computed using the
+     * {@link GC#getCharWidth(char)}.
+     * 
+     * @param gc GC used to perform calculation.
+     * @param t text to modify.
+     * @param width Pixels to display.
+     * @return shortened string that fits in area specified.
+     */
+    public static String getShortStr(GC gc, String t, int width)
     {
         if (t == null || t.equals("")) {
 	    return t;
@@ -69,7 +174,7 @@ public class TextUtils
 	int length = chars.length;
 	int left = 0;
 	int right = length - 1;
-	int calcWidth = gc.stringExtent("...").x;;
+	int calcWidth = gc.stringExtent("...").x;
 
 	while (left < right) {
 	    int step = gc.getCharWidth(chars[left]);
