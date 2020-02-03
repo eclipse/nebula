879b1a730a220a510bc4aeefc52c2fcff21c2499
Laurent Caron <laurent.caron@gmail.com>
2020-02-03 15:07:33 +0100

    Bug 558568 - Include "FontAwesome for SWT" in Nebula

---
+++ widgets/fontawesome/org.eclipse.nebula.widgets.fontawesome.snippets/src/org/eclipse/nebula/widgets/fontawesome/FontAwesomeSnippet.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Patrik Dufresne (http://www.patrikdufresne.com/).
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors: Patrik Dufresne (info at patrikdufresne dot com) - initial API and
+ * implementation
+ *******************************************************************************/
+package org.eclipse.nebula.widgets.fontawesome;
+
+/**
+ * Snippet for the FontAwesome widget
+ */
+public class FontAwesomeSnippet {
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+
+	}
+
+}
