c6293495113bc06cd43989369ae2faddf06f205d
Laurent Caron <laurent.caron@gmail.com>
2020-02-18 14:13:11 +0100

    Bug 560265 - Add e4 theming for TableCombo
    Initial implementation of the e4 application

---
+++ widgets/tablecombo/org.eclipse.nebula.widgets.tablecombo.example.e4/src/org/eclipse/nebula/tablecombo/example/e4/parts/Model.java
@@ -0,0 +1,77 @@
+/****************************************************************************
+ * Copyright (c) 2009 Marty Jones
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *	Marty Jones <martybjones@gmail.com> - initial API and implementation
+ *****************************************************************************/
+
+package org.eclipse.nebula.tablecombo.example.e4.parts;
+
+/**
+ * Snippet Model class
+ *
+ */
+public class Model {
+	private int id;
+	private String description;
+	
+	public Model(int id, String description) {
+		this.id = id;
+		this.description = description;
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Model other = (Model) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public String toString() {
+		return "[id=" + id + "] [description=" + description + "]";
+	}
+}
