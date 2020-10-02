c8144d7b36b2e3f325a3e1f7bea4a722a3cdca3c
Dariusz Michura <dariusz.michura@gmail.com>
2020-10-02 10:51:37 +0200

    Bug: 566603 The text in Text widget may be displayed in 'prompt' mode
    Changed way of applying prompt style
    
    Signed-off-by: Dariusz Michura <dariusz.michura@gmail.com>

--- widgets/opal/promptsupport/org.eclipse.nebula.widgets.opal.promptsupport/src/org/eclipse/nebula/widgets/opal/promptsupport/BaseFocusControlListener.java
+++ widgets/opal/promptsupport/org.eclipse.nebula.widgets.opal.promptsupport/src/org/eclipse/nebula/widgets/opal/promptsupport/BaseFocusControlListener.java
@@ -14,22 +14,20 @@ package org.eclipse.nebula.widgets.opal.promptsupport;
 
 import org.eclipse.nebula.widgets.opal.commons.SWTGraphicUtil;
 import org.eclipse.nebula.widgets.opal.promptsupport.PromptSupport.FocusBehavior;
-import org.eclipse.swt.events.ControlEvent;
-import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.events.FocusListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.widgets.Control;
 
 /**
  * Abstract class that contains code for the FocusLost, FocusGained and
  * ControlResized events
  */
-abstract class BaseFocusControlListener implements FocusListener, ControlListener {
+abstract class BaseFocusControlListener implements FocusListener {
 
 	protected Control control;
-	private boolean firstDraw;
 	private Font initialFont;
 	private Color initialBackgroundColor;
 	private Color initialForegroundColor;
@@ -43,9 +41,6 @@ abstract class BaseFocusControlListener implements FocusListener, ControlListene
 	 */
 	BaseFocusControlListener(final Control control) {
 		this.control = control;
-		storeInitialLook();
-		firstDraw = true;
-		PromptSupport.setPromptDisplayed(control, false);
 	}
 
 	/**
@@ -53,11 +48,12 @@ abstract class BaseFocusControlListener implements FocusListener, ControlListene
 	 */
 	@Override
 	public void focusGained(final FocusEvent e) {
-		if (isFilled()) {
+        if (isFilled()) {
 			// Widget not empty
 			PromptSupport.setPromptDisplayed(control, false);
 			return;
 		}
+        
 		applyInitialLook();
 		PromptSupport.setPromptDisplayed(control, true);
 		if (PromptSupport.getFocusBehavior(control) == FocusBehavior.HIDE_PROMPT) {
@@ -102,7 +98,8 @@ abstract class BaseFocusControlListener implements FocusListener, ControlListene
 	 */
 	@Override
 	public void focusLost(final FocusEvent e) {
-		if (isFilled()) {
+        if (isFilled()) {
+            PromptSupport.setPromptDisplayed(control, false);
 			return;
 		}
 
@@ -124,24 +121,6 @@ abstract class BaseFocusControlListener implements FocusListener, ControlListene
 	protected abstract void fillPromptText();
 
 	/**
-	 * @see org.eclipse.swt.events.ControlListener#controlMoved(org.eclipse.swt.events.ControlEvent)
-	 */
-	@Override
-	public void controlMoved(final ControlEvent e) {
-	}
-
-	/**
-	 * @see org.eclipse.swt.events.ControlListener#controlResized(org.eclipse.swt.events.ControlEvent)
-	 */
-	@Override
-	public void controlResized(final ControlEvent e) {
-		if (firstDraw) {
-			firstDraw = true;
-			focusLost(null);
-		}
-	}
-
-	/**
 	 * Store the initial look of the widget
 	 */
     protected void storeInitialLook() {
@@ -154,7 +133,17 @@ abstract class BaseFocusControlListener implements FocusListener, ControlListene
      * Attach listeners to the control
      */
     void hookControl() {
+        boolean isPromptDisplayed = false;
+        storeInitialLook();
+        
+        if (!control.isFocusControl() && !isFilled()) {
+            applyPromptLook();
+            fillPromptText();
+            isPromptDisplayed = true;
+        }
+
+        PromptSupport.setPromptDisplayed(control, isPromptDisplayed);
+        
         control.addFocusListener(this);
-        control.addControlListener(this);
     }
 }
