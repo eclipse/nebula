b14903df8e271512b684c0e73334df5281824670
Laurent Caron <laurent.caron@gmail.com>
2020-11-01 15:34:17 +0100

    Bug 566500 - VControl : Bad size
    VControl : Compute the size with the width and the height of the
    associated SVG document

--- widgets/cwt/org.eclipse.nebula.cwt/src/org/eclipse/nebula/cwt/svg/SvgFragment.java
+++ widgets/cwt/org.eclipse.nebula.cwt/src/org/eclipse/nebula/cwt/svg/SvgFragment.java
@@ -21,15 +21,20 @@ import org.eclipse.swt.graphics.Rectangle;
 
 /**
  * An svg document can contain one or more svg fragments, each denoted
- * by the "svg" tag.  Each of these consists of all the information 
+ * by the "svg" tag. Each of these consists of all the information
  * necessary to paint a graphic to the screen, including definition
  * and css style child elements.
- * <p>Of particular importance is that the svg fragment can also contain
+ * <p>
+ * Of particular importance is that the svg fragment can also contain
  * a viewbox which can be used for scaling the image to a particular size.
  * Therefore this element is where the real-world rendered dimensions
- * interact with the svg dimenions.</p>
- * <p>See also:
- * <a href="http://www.w3.org/TR/SVG/struct.html#SVGElement">http://www.w3.org/TR/SVG/struct.html#SVGElement</a></p>
+ * interact with the svg dimenions.
+ * </p>
+ * <p>
+ * See also:
+ * <a href=
+ * "http://www.w3.org/TR/SVG/struct.html#SVGElement">http://www.w3.org/TR/SVG/struct.html#SVGElement</a>
+ * </p>
  */
 public class SvgFragment extends SvgContainer {
 
@@ -46,7 +51,7 @@ public class SvgFragment extends SvgContainer {
 	float[] viewBox;
 	SvgTransform boundsTransform;
 	boolean preserveAspectRatio;
-	
+
 	private Map<String, SvgElement> elementMap;
 
 	SvgFragment(SvgContainer container, String id) {
@@ -55,53 +60,64 @@ public class SvgFragment extends SvgContainer {
 		boundsTransform = new SvgTransform();
 		boundsTransform.data = new float[] { 1, 0, 0, 1, 0, 0 };
 	}
-	
+
 	/**
-	 * Apply this svg fragment to the given graphics context, scaled to fit within
-	 * the given bounds.  This method will recursive call the apply methods of all
+	 * Apply this svg fragment to the given graphics context, scaled to fit
+	 * within
+	 * the given bounds. This method will recursive call the apply methods of
+	 * all
 	 * contained svg elements, thereby painting the entire fragment to the given
 	 * graphics context.
-	 * @param gc the graphics context
-	 * @param bounds the bounds to which this fragment will be scaled
+	 * 
+	 * @param gc
+	 *            the graphics context
+	 * @param bounds
+	 *            the bounds to which this fragment will be scaled
 	 */
 	public void apply(GC gc, Rectangle bounds) {
 		boundsTransform.translate(bounds.x, bounds.y);
 
-		if(viewBox != null) {
+		if (viewBox != null) {
 			float sx = bounds.width / viewBox[2];
 			float sy = bounds.height / viewBox[3];
 			boundsTransform.scale(sx, sy);
-		} else if(width != null && height != null){
+		} else if (width != null && height != null) {
 			float sx = bounds.width / width;
 			float sy = bounds.height / height;
 			boundsTransform.scale(sx, sy);
 		}
-		
+
 		super.apply(gc);
-		
+
 		boundsTransform.data = new float[] { 1, 0, 0, 1, 0, 0 };
 	}
-	
+
 	public SvgElement getElement(String id) {
 		return elementMap.get(id);
 	}
-	
+
 	@Override
 	public SvgFragment getFragment() {
 		return this;
 	}
-	
+
 	/**
 	 * Return a map of css styles for the given class name, if it exists.
-	 * <p>Each SvgFragment can contain a style element which consists of css styles.</p>
-	 * @param className the name of the css class to return styles for.
-	 * @return a map of css style for the given class name if it exists, null otherwise.
+	 * <p>
+	 * Each SvgFragment can contain a style element which consists of css
+	 * styles.
+	 * </p>
+	 * 
+	 * @param className
+	 *            the name of the css class to return styles for.
+	 * @return a map of css style for the given class name if it exists, null
+	 *         otherwise.
 	 */
 	public Map<String, String> getStyles(String className) {
 		SvgElement element = elementMap.get("style"); //$NON-NLS-1$
-		if(element instanceof SvgStyle) {
+		if (element instanceof SvgStyle) {
 			Map<String, Map<String, String>> classes = ((SvgStyle) element).styles;
-			if(classes != null) {
+			if (classes != null) {
 				return classes.get(className);
 			}
 		}
@@ -110,7 +126,7 @@ public class SvgFragment extends SvgContainer {
 
 	@Override
 	public float[] getViewport() {
-		if(x == null || y == null) {
+		if (x == null || y == null) {
 			return new float[] { 0, 0, width, height };
 		} else {
 			return new float[] { x, y, width, height };
@@ -119,7 +135,9 @@ public class SvgFragment extends SvgContainer {
 
 	/**
 	 * Returns true if this fragment contains an SvgElement with the given id.
-	 * @param id the id of the element
+	 * 
+	 * @param id
+	 *            the id of the element
 	 * @return true if the element exists, false otherwise
 	 */
 	public boolean hasElement(String id) {
@@ -128,21 +146,34 @@ public class SvgFragment extends SvgContainer {
 
 	/**
 	 * Returns true if this SvgFragment is at the outermost level, meaning it
-	 * is a direct child of the SvgDocument.  This is an important distinction
-	 * because, as with all svg elements, fragments can be nested.  Each svg
+	 * is a direct child of the SvgDocument. This is an important distinction
+	 * because, as with all svg elements, fragments can be nested. Each svg
 	 * fragment will establish a new coordinate system, but only the outer
-	 * fragment will determine the scaling necessary to display at the requested size.
+	 * fragment will determine the scaling necessary to display at the requested
+	 * size.
+	 * 
 	 * @return true if this fragment is at the outermost level, false otherwise.
 	 */
 	public boolean isOutermost() {
 		return getContainer() == null;
 	}
-	
+
 	void put(SvgElement element) {
 		String id = element.getId();
-		if(id != null) {
+		if (id != null) {
 			elementMap.put(id, element);
 		}
 	}
-	
+
+	public float[] getViewBox() {
+		return viewBox;
+	}
+
+	public Float getWidth() {
+		return width;
+	}
+
+	public Float getHeight() {
+		return height;
+	}
 }
